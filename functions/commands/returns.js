c7a47c8a7f3365f1e374c774ab4c232cb23e481e
shelling <navyblueshellingford@gmail.com>
2017-07-06 13:54:44 -0700

    New commands
    - Fetch data from metabase with superagent
    - Add returns command
    - Add orders command (Richard)

---
+++ functions/commands/returns.js
@@ -0,0 +1,50 @@
+const lib = require('lib')({token: process.env.STDLIB_TOKEN});
+const metabase = {
+    email: process.env.METABASE_USER,
+    password: process.env.METABASE_PASS
+};
+
+/**
+* /hello
+*
+*   Basic "Hello World" command.
+*   All Commands use this template, simply create additional files with
+*   different names to add commands.
+*
+*   See https://api.slack.com/slash-commands for more details.
+*
+* @param {string} user The user id of the user that invoked this command (name is usable as well)
+* @param {string} channel The channel id the command was executed in (name is usable as well)
+* @param {string} text The text contents of the command
+* @param {object} command The full Slack command object
+* @param {string} botToken The bot token for the Slack bot you have activated
+* @returns {object}
+*/
+module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
+
+    var superagent = require('superagent');
+    superagent
+        .post("http://metabase-757206338.us-west-2.elb.amazonaws.com/api/session")
+        .send(metabase)
+        .set('Content-Type', 'application/json')
+        .end(function(err, res) {
+            if (res.status === 200) {
+                superagent
+                    .post("http://metabase-757206338.us-west-2.elb.amazonaws.com/api/card/5/query/json")
+                    .set('X-Metabase-Session', `${JSON.parse(res.text).id}`)
+                    .end(function(err, res) {
+
+                        callback(null, {
+                            response_type: 'in_channel',
+                            text: `hello, <@${user}>...\nyou said: ${text}, ${res.text}`
+                        });
+
+                    })
+            } else {
+                callback(null, {
+                    response_type: 'in_channel',
+                    text: `${res.text}`
+                })
+            }
+        })
+};
